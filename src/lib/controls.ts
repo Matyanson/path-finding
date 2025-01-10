25f0ebe7eedb7791f313f67dcbdc6fab51c50bd1
Matyanson <matyas.perem@seznam.cz>
2025-01-11 00:37:37 +0100

    feat: start and end points

--- src/lib/controls.ts
+++ src/lib/controls.ts
@@ -1,17 +1,32 @@
 import { get } from "svelte/store";
-import { canvasState, dotSpacing } from "./store";
+import { canvasState, dotSpacing, finishIndex, points, startIndex } from "./store";
 
 export function drawCanvas() {
     const { canvas, ctx } = get(canvasState);
 
-    // draw pionts
+    // draw coord points
     ctx.fillStyle = "#aaa";
     for (let y = get(dotSpacing) / 2; y < canvas.height; y += get(dotSpacing)) {
         for (let x = get(dotSpacing) / 2; x < canvas.width; x+= get(dotSpacing)) {
-            ctx.beginPath();
-            ctx.arc(x, y, 3, 0, 2 * Math.PI);
-            ctx.fill();
-            ctx.closePath();
+            drawPoint(ctx, x, y, 3);
         }
     }
+    
+    // draw start and finish points
+    const startCoords = get(points)[get(startIndex)];
+    const finishCoords = get(points)[get(finishIndex)];
+
+    ctx.fillStyle = "#0a0";
+    drawPoint(ctx, startCoords.x, startCoords.y, 10);
+    ctx.fillStyle = "#a00";
+    drawPoint(ctx, finishCoords.x, finishCoords.y, 10);
+
+}
+
+/* HELPER FUNCTIONS */
+function drawPoint(ctx: CanvasRenderingContext2D, x: number, y: number, r: number) {
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.closePath();
 }
--- src/lib/store.ts
+++ src/lib/store.ts
@@ -1,8 +1,19 @@
 import { writable } from "svelte/store";
 
+type Coords = {
+    x: number,
+    y: number
+}
+
+type Point = {
+    coords: Coords
+}
+
 export const canvasState = writable<{ canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D }>();
 export const dotSpacing = writable(50);
-
+export const points = writable<Coords[]>([{x: 25, y: 25}, {x: 525, y: 525}]);
+export const startIndex = writable(0);
+export const finishIndex = writable(1);
 
 export function setCanvas(canvas: HTMLCanvasElement) {
     const ctx = canvas.getContext('2d');
