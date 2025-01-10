00795a12263d7275ef29deb8d8f13c3934377ff7
Matyanson <matyas.perem@seznam.cz>
2025-01-10 22:55:37 +0100

    feat: full-page canvas

---
+++ src/lib/components/Playground.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+
+
+    let canvas: HTMLCanvasElement;
+
+    function onResize() {
+        canvas.width = document.documentElement.clientWidth;
+        canvas.height = document.documentElement.clientHeight;
+
+        console.log(canvas.width, canvas.height);
+    }
+
+    onMount(onResize);
+
+</script>
+
+<canvas bind:this={canvas}></canvas>
+<svelte:window on:resize={onResize} />
+
+<style>
+     canvas {
+        display: block;
+     }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    import Playground from "$lib/components/Playground.svelte";
+
+</script>
+
+<Playground />
