56c708643caad114ce3e4a785b5d387cd58b9e28
Matyanson <matyas.perem@seznam.cz>
2025-01-11 13:22:57 +0100

    feat: mouse controller

--- src/lib/components/Playground.svelte
+++ src/lib/components/Playground.svelte
@@ -1,10 +1,14 @@
 <script lang="ts">
     import { drawCanvas } from "$lib/controls";
     import { canvasState, setCanvas } from "$lib/store";
+    import type { Coords } from "$lib/model";
+    import { createMouseController } from "$lib/mouseController";
+    import { setCanvas } from "$lib/store";
     import { onMount } from "svelte";
 
 
     let canvas: HTMLCanvasElement;
+    const mouseController = createMouseController();
 
     function onResize() {
         canvas.width = document.documentElement.clientWidth;
@@ -15,11 +19,16 @@
     }
 
     onMount(() => {
+        mouseController.init(canvas);
+
         // store canvas globally
         setCanvas(canvas);
         onResize();
-    });
 
+        return () => {
+            mouseController.destroy();
+        }
+    });
 </script>
 
 <canvas bind:this={canvas}></canvas>
---
+++ src/lib/mouseController.ts
@@ -0,0 +1,53 @@
+import { get, writable } from "svelte/store";
+import type { Coords } from "./model";
+
+
+
+export function createMouseController() {
+    const position = writable<Coords>({ x: 0, y: 0 });
+    const deltaPosition = writable<Coords>({ x: 0, y: 0 });
+    const isDown = writable(false);
+    const abortController = new AbortController();
+    let targetElement: HTMLElement;
+
+    function init(el: HTMLElement) {
+        targetElement = el;
+        window.addEventListener("mousemove", onMouseMove, { signal: abortController.signal });
+        window.addEventListener("mousedown", onMouseDown, { signal: abortController.signal });
+        window.addEventListener("mouseup", onMouseUp, { signal: abortController.signal });
+    }
+
+    function onMouseMove(e: MouseEvent) {
+        if(!get(isDown)) return;
+
+        const { clientX, clientY } = e;
+        const { x, y } = get(position);
+        const delta: Coords = {
+            x: clientX - x,
+            y: clientY - y
+        }
+        deltaPosition.set(delta);
+        position.set({ x: clientX, y: clientY });
+    }
+
+    function onMouseDown(e: MouseEvent) {
+        const { clientX, clientY } = e;
+        position.set({ x: clientX, y: clientY });
+        isDown.set(true);
+    }
+
+    function onMouseUp(e: MouseEvent) {
+        isDown.set(false);
+    }
+
+    function destroy() {
+        abortController.abort();
+    }
+
+    return {
+        position,
+        deltaPosition,
+        init,
+        destroy
+    }
+}
